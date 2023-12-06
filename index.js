b98976f17daca545d760c9ee4f72c6a045cc0cbd
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-12-06 22:22:08 +0530

    setup swagger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config()
 import express from 'express';
 import cors from 'cors';
+import swagger from 'swagger-ui-express';
+import apiDocs from './swagger.json' assert {type:'json'};
 
 import connectDB from './db/mongoose.js';
 import { errorHandlerMiddleware } from './middlewares/error_handler.js';
@@ -12,6 +14,9 @@ import taskRouter from './routes/task.route.js';
 
 const app = express();
 
+// api documentation
+app.use('/api/docs', swagger.serve, swagger.setup(apiDocs));
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
@@ -19,7 +24,7 @@ app.use(cors({
 }))
 
 app.get('/', (req, res)=> {
-    res.send('Welcome');
+    res.send('<h1>Task Manager API</h1><a href="/api/docs">Documentaion</a>');
 })
 
 
